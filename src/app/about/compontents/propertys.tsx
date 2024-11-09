aa681ebee834aa4c761416994bae618b226ef04b
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-11-09 17:41:32 -0300

    fix: change Property component

--- src/app/about/compontents/propertys.tsx
+++ src/app/about/compontents/propertys.tsx
@@ -8,20 +8,21 @@ const getPropertys = async () => {
 };
 
 const Propertys = async () => {
-  // const data = await getPropertys();
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: {
-      revalidate: 0,
-    },
-  }).then((res) => res.json());
+  const data = await getPropertys();
+  //   const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //     next: {
+  //       revalidate: 0,
+  //     },
+  //   }).then((res) => res.json());
 
   console.log(data);
 
   return (
     <div>
-      {data.map((i: any) => (
+      <p>property</p>
+      {/* {data.map((i: any) => (
         <p key={i.id}>{i.title}</p>
-      ))}
+      ))} */}
     </div>
   );
 };
